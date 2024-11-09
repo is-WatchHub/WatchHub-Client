95a60e28d15c4b4c84c871b8f084db3f05ad2243
georgedem <georgiidemyanov@yandex.ru>
2024-11-10 02:09:52 +0300

    (#16) feat: config

---
+++ watchhub-client/src/config.js
@@ -0,0 +1,17 @@
+const config = {
+    images: [
+        "https://avatars.mds.yandex.net/i?id=57cc99ccf948592efd9429fc403e80ad23ffde82-4360790-images-thumbs&n=13",
+        "https://avatars.mds.yandex.net/i?id=44e6fe9397bcbfa12a8af9d91bd16dcb0090f853-5467261-images-thumbs&n=13",
+        "https://m.media-amazon.com/images/M/MV5BMzNhNTE0NWQtN2E1Ny00NjcwLTg1YTctMGY1NmMwODJmY2NmXkEyXkFqcGc@._V1_.jpg",
+        "https://avatars.mds.yandex.net/i?id=fa86eedcfe2349dbb523e37f8c847c2c44418103-8209530-images-thumbs&n=13",
+        "https://avatars.mds.yandex.net/i?id=0f41449c21d5d95e52bdf02cc16b7069d34019bf-5470326-images-thumbs&n=13",
+        "https://avatars.mds.yandex.net/i?id=7b6e683cb44af5ac49e9cbe895c90df659494b7bc0fd8684-12606451-images-thumbs&n=13"
+    ],
+    trailers: [
+        "https://imdb-video.media-imdb.com/vi2629352217/1434659607842-pgv4ql-1526462823842.mp4?Expires=1731247811&Signature=UGKTu44rU5~7dSkyfcXU0n1~Uz~MPfCoazh9a-w7wDRq293-22t3~kf7MEYjwkWnQZ~-FxfwTur~JxoWjSabjqjZdznnx~hVCO~wU0vP72Nr9ojPt~-En79aEmTzpmjGbbVFiI1vNahmkRTPGyIeB96c7uT-d1qRvxbLiTnPKek-yMpPIhCXYugD1Z0DHVSQ7Cw2sD9uethCNY9KRWOVWa0s1iyiisyqfNLCPRFX4HjYmIiBLuVW4GxOiDXOkHj4nHyHGrn8by1Gkt9cBtgFJ8dU0xBg4IyNEdy261-7efPE2yCyCQiew8a~kdyj9seSn3lB7wAlBFgnakoF9YoA2g__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        "https://imdb-video.media-imdb.com/vi3886594585/1434659607842-pgv4ql-1538759423661.mp4?Expires=1731248402&Signature=bVoL6MOAwXgk~AxEvC~~1vhpRV9C8D4dlr4uHUByYBcxDehsQ~FKV-3dko436siVdAv0Dh87Zsgb0Se7Ndhb9V3AxNOUCXwIg6gS2ZEnxOpd6bovWiIX92en04b3Ogg1HdKBDaIYKzdVMrngOu~IY7LA8r-L~YBpc9n04U23MuMDAsPZHgAz8741MktnnJA4QaLCKYw1rYhmAtY3T8uPn2MO0BeshserxNpzEsAXN8-~hwGROoFB7mmCF-uSthsD2hOc0wFHj9F48G-jNOlqryJE9ZfJ-v0m5rxI3jwcONgUPnQAsuG1mqIKvwzgfA2dUfwWrKs95LpN1YAlrq7w~g__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        "https://imdb-video.media-imdb.com/vi447330841/1434659607842-pgv4ql-1538759607373.mp4?Expires=1731248984&Signature=Y09~yaIULFr~93kKf7GO1n4wq5ecj8HlZ4BqyiTe4Fivt~xa5uhJRTr84ioDd6ntpVs8NI4UIpsScpcwZmEFpDFhXUr-gqoW9E753FDaRioJKAV9vqW3WDFLUhprfQpVgsKOLGEDnCU0uAnHje7SlM7C0BSM4cE6s9LBSPa0Hqfm3wWW7vuBPDj~PyOyaOsv~IfzDcLrlcLhxczxFUxKXBREEKHai33EZ10HAxIKePpeXmpE9BARiJ0n5TqMgg6uFJX7q8DT2gAACf82roSq5HzeV4sRRR5~8TveB3tQixV1JYryyJfaixP5YwaETXFL5OKdFBv3VDsisILnSjmQ3g__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"
+    ]
+};
+
+export default config;
