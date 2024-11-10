1f068def873d51fcc4bb573c2a0284a02ba3c17d
georgedem <georgiidemyanov@yandex.ru>
2024-11-11 01:25:03 +0300

    (#17) update: tests

--- watchhub-client/src/App.test.js
+++ watchhub-client/src/App.test.js
@@ -1,18 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import Home from './pages/Home';
-import Movies from './pages/Movies';
 import Profile from './pages/Profile';
 
 test('renders Home page description', () => {
   render(<Home />);
   const homeDescription = screen.getByText(/WatchHub — это онлайн-кинотеатр/i);
   expect(homeDescription).toBeInTheDocument();
-});
-
-test('renders Movies page', () => {
-  render(<Movies />);
-  const moviesElement = screen.getByText(/Movies Page/i);
-  expect(moviesElement).toBeInTheDocument();
 });
 
 test('renders Profile page', () => {
