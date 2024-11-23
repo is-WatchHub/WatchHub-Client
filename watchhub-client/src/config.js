7ee91e5984e183a65cc2465cf721a97c2986defb
georgedem <georgiidemyanov@yandex.ru>
2024-11-23 03:06:37 +0300

    (#20) fix: links

--- watchhub-client/src/config.js
+++ watchhub-client/src/config.js
@@ -8,9 +8,9 @@ const config = {
         "https://avatars.mds.yandex.net/i?id=7b6e683cb44af5ac49e9cbe895c90df659494b7bc0fd8684-12606451-images-thumbs&n=13"
     ],
     trailers: [
-        "https://imdb-video.media-imdb.com/vi2629352217/1434659607842-pgv4ql-1526462823842.mp4?Expires=1732404659&Signature=pdfEBdwLLRrwc-E-G1S-MitMbjvCxKms52PPM-yBpBnza65bEz8RDOyVfwznKcdEVYPDu-3YvZnK3qsuSKm29AnslEBKPINAt1FB5rz3jRfkHa36qrD1E10k97pAcUzFQxWMu~9llwV0CvVG8FGyL4P5EZYM4zICWs4Wib90rcxsgAIJOcEyre0XIkBUVOP8FYtIxmMSi8CzU5O0-mDt8uyI263AD~bWDcVlt2MQIzI8zD~97D~M3m2pDGcZ-sZO~zGCs9v90qKgNGg9yaEfSwQEAkY52kIaiXwm9i79fFmj4oac6ckYCewMW1ekg5x3aOWQnxq9ajzYXdhSdxVfYQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
-        "https://imdb-video.media-imdb.com/vi3886594585/1434659607842-pgv4ql-1538759423661.mp4?Expires=1732404762&Signature=V~ioOD-gUnIlA3adpnP6wzto2SAoQ7sMt6QjqMvswf1Bc59YNgRBJm24lO7JxEF3CSyyM0CRraf~4FzGcU2uhAoJE49Z0oQzf5jqPcgZa-aamWumixutOgQcQilU8K5KvGjsE7D4jU~97fgEx3QdLHqysrWqtgWtUXLXInaNbl51XxzvjN9XOxmGbRbcUnrcZhviy-a27JVqfI0eQMwwd99bsYa-6Qt6zMC8DjoA58xjDg26s3SOL3x7AFpkAyt1oFr4UOuEwnnFYiMXPd46mB8oi0l5jMSKzV~5IqZSpvBmLi2-OhjQP-EwZgR9h~gC9IqweZplba~V2yJ-p~rWhw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
-        "https://imdb-video.media-imdb.com/vi447330841/1434659607842-pgv4ql-1538759607373.mp4?Expires=1732404790&Signature=rrX-0RQwlstYdPfUrz5~VZhctbNSHdGup55AFeGa6VJGx-qZ4isoKOddTmRJ7srYZSPWXmp30IahmSRgy-RqcZJZGj71lBY9PcK6fTkInUR3EEfscTSZE1-IIei7LplvBvnPBJ9pFTC90n3NLfQsR6uDyJ5KRWu0NIao9nOoT2Hi5uunFAUeN6WK34BRA45oBsMmw6c5TonztwOICgI9218nb15kQFiao9up4Tb-zR6EhopwGp2vOdGgxwzt7~FDeaaK-VMH6KCnyJLOLBwaYolt-HDqmIVcz~oLCQqTT7ZWU07GXFtDx21RfXG0PXQpTUzcpWb0Lywys8md-rj1Iw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"
+        "",
+        "",
+        ""
     ],
     serverUrl: "https://watchhub-jjji.onrender.com"
 };
